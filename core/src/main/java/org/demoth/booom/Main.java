7d37733ebcef3209238cb5fb841a8790ecebc7d8
demoth <bubnov.d.e@gmail.com>
2024-09-11 15:54:58 +0200

    add objects

--- core/src/main/java/org/demoth/booom/Main.java
+++ core/src/main/java/org/demoth/booom/Main.java
@@ -1,37 +1,37 @@
 package org.demoth.booom;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private Texture background;
+    private Texture objects;
 
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("background-forest.png");
+        background = new Texture("background-forest.png");
+        objects = new Texture("objects-no-bg.png");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 0, 0);
+        batch.draw(background, 0, 0);
+        batch.draw(objects, 0, 0);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        background.dispose();
+        objects.dispose();
     }
 }
